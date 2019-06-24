d3bd9a79c4ba9d13e3aca9f136ab68f0206d2a24
Citlally Ruiz <ciciiiruiz@gmail.com>
2019-06-24 10:43:07 -0500

    Initial Commit

---
+++ to_do_list/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  to_do_list
+//
+//  Created by Apple on 6/24/19.
+//  Copyright © 2019 Citlally Ruiz. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
