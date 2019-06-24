72fd89099cd4444fbbaf54a778b16714e664161d
Citlally Ruiz <ciciiiruiz@gmail.com>
2019-06-24 13:23:35 -0500

    Complete Iteration 0

---
+++ to_do_list/ToDo.swift
@@ -0,0 +1,15 @@
+//
+//  ToDo.swift
+//  to_do_list
+//
+//  Created by Apple on 6/24/19.
+//  Copyright © 2019 Citlally Ruiz. All rights reserved.
+//
+
+import UIKit
+
+class ToDo {
+    var name = ""
+    var important = false
+    
+}
